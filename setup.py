b22dba1a820bd9da09624b7c5ac16c209e6630a5
Brian J Petersen <brianjpetersen@gmail.com>
2015-03-17 09:47:03 -0400

    Reverting commit.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ pass
 # first party libraries
 pass
 
+<<<<<<< HEAD
 def load_file(fname):
     try:
         with open(fname, 'rb') as f:
@@ -20,6 +21,17 @@ license = load_file('LICENSE.md')
 setuptools.setup(
     name = 'tiny_id',
     version = version,
+=======
+try:
+    with open('README.md') as f:
+        readme = f.read()
+except:
+    readme = None
+
+setuptools.setup(
+    name = 'dotedict',
+    version = '0.0.1',
+>>>>>>> parent of 763f213... Up revision, with slight refactoring for improved versioning and
     description = readme,
     license = license,
     author = 'Brian Petersen',
--- tiny_id/__init__.py
+++ tiny_id/__init__.py
@@ -1,8 +1,9 @@
 # standard libraries
-import os
+pass
 # third party libraries
 pass
 # first party libraries
+<<<<<<< HEAD:tiny_id/__init__.py
 from tiny_id import random
 
 _FILE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
@@ -12,3 +13,6 @@ try:
         __version__ = f.read()
 except:
     pass
+=======
+from dotedict import DoteDict
+>>>>>>> parent of 763f213... Up revision, with slight refactoring for improved versioning and:dotedict/__init__.py
